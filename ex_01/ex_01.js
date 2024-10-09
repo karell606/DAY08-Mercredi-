ae89ed36deb4eff2a8bd87d66402bda6abb0e8c3
karell606 <karell.azabe@epitech.digital>
2024-10-09 15:13:58 +0200

    ex_01.js

---
+++ ex_01/ex_01.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#latest-creations-carousel', {
+        type       : 'loop',
+        perPage    : 3,
+        autoplay   : true,
+        interval   : 5000,
+        focus      : 'center',
+        breakpoints: {
+            768: {
+                perPage: 1,
+            },
+            1024: {
+                perPage: 2,
+            },
+        },
+    }).mount();
+});
